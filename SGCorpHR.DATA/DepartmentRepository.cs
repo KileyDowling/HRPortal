c3d85643aa3d7553e81cc87d67bab2aa2f154827
KileyDowling <KileyDowling@gmail.com>
2015-07-30 08:59:52 -0400

    Department CRUD repo & tests complete

---
+++ SGCorpHR.DATA/DepartmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using SGCorpHR.Models;
+
+namespace SGCorpHR.DATA
+{
+    public class DepartmentRepository
+    {
+        public List<Departments> ListAll()
+        {
+            using (SqlConnection cn = new SqlConnection(Settings.ConnectionString))
+            {
+                return cn.Query<Departments>("Select * from Departments").ToList();
+            }
+        }
+
+        public Departments GetSingleDpt(int departmentId)
+        {
+            using (SqlConnection cn = new SqlConnection(Settings.ConnectionString))
+            {
+                return cn.Query<Departments>("Select * from Departments WHERE DepartmentID = @dptId", new { dptId = departmentId }).ToList().FirstOrDefault();
+            }
+        }
+
+        public void CreateDepartment(string departmentName)
+        {
+            using (SqlConnection cn = new SqlConnection(Settings.ConnectionString))
+            {
+                cn.Query<Departments>("insert into Departments(DepartmentName) values('" + departmentName +"')");
+            }
+        }
+    }
+}
---
+++ SGCorpHR.TEST/DepartmentRepoTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SGCorpHR.DATA;
+
+namespace SGCorpHR.TEST
+{
+    [TestFixture]
+    public class DepartmentRepoTests
+    {
+        [Test]
+        public void ListAllDptTest()
+        {
+            var repo = new DepartmentRepository();
+            var dptList = repo.ListAll();
+            Assert.AreEqual(5,dptList.Count);
+        }
+
+        [Test]
+        public void GetSingleDptTest()
+        {
+            var repo = new DepartmentRepository();
+            var dpt = repo.GetSingleDpt(4);
+            Assert.AreEqual("Sales", dpt.DepartmentName);
+
+        }
+
+        [Test]
+        public void CreateDepartmentTest()
+        {
+            var repo = new DepartmentRepository();
+           // repo.CreateDepartment("Talent Development");
+            var dptList = repo.ListAll();
+            var newDpt = dptList.FirstOrDefault(x => x.DepartmentID == 6);
+            Assert.AreEqual(6,dptList.Count);
+            Assert.AreEqual("Talent Development", newDpt.DepartmentName);
+        }
+    }
+}
